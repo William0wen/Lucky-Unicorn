effdfe1872dcb65d31bd021a2d2ab95708a39a16
William Owen <98859927+William0wen@users.noreply.github.com>
2022-03-23 13:25:02 +1300

    instructions

---
+++ 03_instructions_v1.py
@@ -0,0 +1,41 @@
+"""Took function from component 03_v1 as the basis for this
+new function which incorporates both yes/no and show instructions."""
+
+
+# yes/no checking function
+def yes_no(question_text):
+    while True:
+
+        # Ask if played before
+        answer = input(question_text).lower().strip()
+
+        # If yes, program continues
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If no, display instructions
+        elif answer == "no" or answer == "n":
+            answer = "No"
+            return answer
+
+        # Else, show error
+        else:
+            print("\nPlease answer [yes] or [no].")
+
+
+# display instructions function
+def instructions():
+    print("\n\t**** How to Play ****"
+          "\n\tThe rules of the game go here"
+          "\n\nProgram continues\n")
+
+
+# Main
+
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program continues")
