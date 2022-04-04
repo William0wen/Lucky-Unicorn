830600274eae2be1b448f16defc39a2152b8bc80
William Owen <98859927+William0wen@users.noreply.github.com>
2022-04-05 09:21:47 +1200

    update

---
+++ 06_rounds_v1.py
@@ -0,0 +1,48 @@
+"""Component 4 - game mechanics and looping v1
+Based on 05_token_generator_03 but hard-wired to only allocate donkeys
+Gives the user feedback about number of rounds and maintains balance
+Test amount set to $5"""
+
+import random
+
+# Main
+TEST_AMOUNT = 5
+balance = TEST_AMOUNT
+
+rounds_played = 0
+play_again = ""
+
+while play_again != "x" and balance != 0:
+    rounds_played += 1
+    number = random.randint(6, 36)
+
+    # Adjust balance
+    # If number is between 1 and 5
+    # user gets unicorn
+    if 1 <= number <= 5:
+        token = "unicorn"
+        balance += 4
+
+    elif 6 <= number <= 36:
+        token = "donkey"
+        balance -= 1
+
+    else:
+        # if even number, zebra
+        if number % 2 == 0:
+            token = "zebra"
+            balance -= .5
+
+        else:
+            token = "horse"
+            balance -= .5
+
+    print(f"Round {rounds_played}: Token: {token}, balance: ${balance:.2f}.")
+    play_again = input("Do you want to play another round?\n<enter> to play again or [x] to exit\n").lower().strip()
+
+
+# output
+print(f"\nStarting balance: ${TEST_AMOUNT}.")
+print(f"Final balance: ${balance:.2f}.")
+
+
